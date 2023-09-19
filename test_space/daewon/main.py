5702d32f85af0014d32d6662e0f83b0330790fe1
최대원 <empty>
2023-09-19 11:56:38 +0900

    pybullet 실행 docker file - conda를 통해서 python 실행 하나 굳이 바꿀필요 없을듯함

--- test_space/daewon/main.py
+++ test_space/daewon/main.py
@@ -3,8 +3,11 @@ import pybullet as p
 import pybullet_data
 
 if __name__ == "__main__":
+
+    # region pybullet
     p.connect(p.GUI)
-    pandaUid = p.loadURDF(os.path.join(pybullet_data.getDataPath()), "franka_panda/panda.urdf", useFixedBase=True)
+    pandaUid = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "franka_panda/panda.urdf"),useFixedBase=True)
 
     while True:
         p.stepSimulation()
+    # endregion
